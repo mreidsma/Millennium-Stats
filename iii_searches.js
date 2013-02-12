c11bc1366f02de54740071bc3dff42618060d123
Matthew Reidsma <matthew@reidsrow.com>
2013-02-12 14:20:10 -0500

    Tweaked problem with non-search pages

--- iii_searches.js
+++ iii_searches.js
@@ -1,50 +1,54 @@
-$("form").submit(function() {
+// Change this to the directory where you saved iiiwrite.php and either mysqlconnect.php or iiidata.csv
+	var path = "http://gvsulib.com/labs/iiistats/"; // NEEDS A TRAILING SLASH!
 
 	// Millennium doesn't give me the stats on usage I want. That's fine.
-	// I'll do it myself!
+	// I'll do it myself! <-- Quoting my 2.5 year old
 
-	var query = "";
 	var ts = Math.round((new Date()).getTime() / 1000); // Timestamp
-	var datastring = "";
-	
+	var datastring, searchQuery, query;
+
 	// Get value of search field
 	
-	// Keyword
-	var searchQuery = $(this).children('input:text[name=searcharg]').val();
-	
+	if(document.getElementById("searcharg").length > 0 { // Keyword
+		searchQuery = document.getElementById("searcharg").value;
+	}
+
 	// If another search screen grab 
-	if(searchQuery == undefined) {
+	if(searchQuery == "") {
 		// Specific search pages
-		var searchQuery = $('#SEARCH').val();
+		searchQuery = document.getElementById("SEARCH").value;
 	}
 	
 	// Need to write code to capture the Advanced Search
 	
-	if(searchQuery != undefined) {
+	if(searchQuery != "") {
+	
+		searchQuery = encodeURI(searchQuery);
 	
 		// Get search type - could also grab from the URL and parse into readable value
-		var searchy = $("h2").text().trim().split(" ");
-		var searchType = searchy[0];
+		
+		function getSelectedText(elementId) {
+			var elt = document.getElementById(elementId);
 
-			if(searchType == "") {
-				searchy = $("#searchtype").find("option:selected").text().trim().split(" ");
-				searchType = searchy[0];
-			}
+			if (elt.selectedIndex == -1)
+			return null;
+
+			return elt.options[elt.selectedIndex].text;
+		}
+
+		var searchType = encodeURI(getSelectedText('searchtype'));
+		var searchScope = encodeURI(getSelectedText('searchscope'));
+		// Record the search and other details by doing a get request.
 		
-		// Record the search and other details
-		// For now, into a .csv file for ease
-		//
-		// Headers: Timestamp, Search Type, Query
+		datastring = datastring + ts + "/" + searchType + "/" + searchScope + "/" + searchQuery;
+		datastring = path + datastring;
 		
-		datastring = datastring + ts + "," + searchType + "," + searchQuery;
-		datastring = "data=" + datastring;
-		$.ajax({
-			dataType: "string",
-			type: "POST",
-			url: "http://gvsulib.com/temp/iii_write.php",
-			data: datastring
-		});
-		datastring = "";
+		// Silly hack to play nice with any browser. (I'm looking at you, IE and your fincky support of Cross-domain requests.
+		
+		var iFrame = document.createElement('iframe');
+		iFrame.src = datastring;
+		iFrame.width = '0';
+		iFrame.height = '0';
+
+		document.body.appendChild(iFrame);
 	}
-	
-});
--- iii_write.php
+++ iii_write.php
@@ -1,9 +1,47 @@
-<?
-$data = trim($_POST['data']);
+<?php
+	header("Access-Control-Allow-Origin: *");
+	
+	// SETUP
+	
+	// Set this to true if you want to use the MySQL database. If you just want a CSV file, leave it as false.
+	$database = false; 
+	
+	// NO NEED TO EDIT BELOW
 
-if (!$DataFile = fopen("iii_searches.csv", "a")) {echo "Failure: cannot open file"; die;};
-if (!fwrite($DataFile, $data)) {echo "Failure: cannot write to file"; die;};
-fclose($DataFile);
-echo "file write successful";
+	$navString = $_SERVER['REQUEST_URI']; 
+	// Returns "/pathto/script/LinkPosition/Type/Page/"
+
+	$parts = explode('/', $navString); // Break into an array
+
+	// Grab fixed position fields
+	//$parts[3] = Timestamp of search
+	//$parts[4] = Search type (e.g. Keyword, Title...)
+	//$parts[5] = Search query
+
+	// WHOA THIS IS SOME CLUMSY CODE LET'S HOPE IT WORKS
+	
+	if($database == true) { // Save to MySQL database
+	
+	include(mysqlconnect.php); // Connect to database
+	
+	// Save record to database
+	
+	$result = mysql_query("INSERT INTO iii_searches VALUES ('', '$parts[3]', '$parts[4]', '$parts[5]')");
+		if(!$result) {
+			echo "There was a problem.";
+		}
+	
+	} else { // Save to CSV file
+	
+	$data = array($parts[3],$parts[4],$parts[5],$parts[6],$parts[7],$parts[8]);
+	
+	// More sane CSV code thanks to Michael Oehrlich, oehrlich@thulb.uni-jena.de
+	
+	if (!$DataFile = fopen("iiidata.csv", "a")) {echo "Failure: cannot open file"; die;};
+	if (!fputcsv($DataFile, $data)) {echo "Failure: cannot write to file"; die;};
+	fclose($DataFile);
+	echo "file write successful";
+	
+	}
 
 ?>
